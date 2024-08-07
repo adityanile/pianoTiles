385be0492a37a39e611c003381fa9068ad279047
Aditya Nile <adityanile@gmail.com>
2024-08-07 23:04:15 +0530

    Syncing of Piano Tunes

--- Assets/Scripts/KeyManager.cs
+++ Assets/Scripts/KeyManager.cs
@@ -10,12 +10,27 @@ public class KeyManager : MonoBehaviour
     AudioSource audioSource;
     public float pitch;
 
+    public string left;
+    public string right;
+
     private void Start()
     {
+        gameObject.tag = "White";
+
         anim = transform.parent.GetComponent<Animation>();
         pianoManager = GameObject.FindObjectOfType<PianoManager>();
 
         audioSource = pianoManager.gameObject.GetComponent<AudioSource>();
+
+        RaycastHit2D rhit = Physics2D.Raycast(transform.parent.position, Vector2.right, 1);
+        
+        if (rhit)
+           right = rhit.collider.tag;
+
+        RaycastHit2D lhit = Physics2D.Raycast(transform.parent.position, Vector2.left, 1f);
+
+        if (lhit)
+            left = lhit.collider.tag;
     }
 
     private void OnMouseDown()
--- Assets/Scripts/PianoManager.cs
+++ Assets/Scripts/PianoManager.cs
@@ -10,7 +10,9 @@ public class PianoManager : MonoBehaviour
     public List<AudioClip> BaseNores;
     private AudioSource audioSource;
 
-    public float offset = 0.1f;
+    public float upOffset = 0.1f;
+    public float downOffset = 0.1f;
+    public float midOffset = 0.05946f;
 
     // Start is called before the first frame update
     void Start()
@@ -29,19 +31,25 @@ public class PianoManager : MonoBehaviour
     {
         int n = keys.IndexOf(mid);
 
-        Debug.Log(n);
-
-        audioSource.clip = BaseNores[n];
+        audioSource.clip = BaseNores[n % BaseNores.Count];
         mid.pitch = 1;
-        
-        for(int i = 0; i < n; i++)
+
+        for (int i = 0; i < n; i++)
         {
-            keys[n - i - 1].pitch = mid.pitch - offset*(i + 1);
+            // Going down in the piano
+            if (keys[n - i].left.Equals("White"))
+                keys[n - i - 1].pitch = keys[n - i].pitch / midOffset;
+            else
+                keys[n - i - 1].pitch = keys[n - i].pitch / downOffset;
         }
 
-        for(int i = n+1, j = 1; i < keys.Count; i++, j++)
+        for (int i = n + 1, j = 1; i < keys.Count; i++, j++)
         {
-            keys[i].pitch = mid.pitch + offset*(j);
+            // Going up in the piano
+            if (keys[i - 1].right.Equals("White"))
+                keys[i].pitch = keys[i - 1].pitch * midOffset;
+            else
+                keys[i].pitch = keys[i - 1].pitch * upOffset;
         }
 
         allocated = true;
